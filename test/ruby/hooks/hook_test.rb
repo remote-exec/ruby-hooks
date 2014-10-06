0c6a523f8d3a8fde5cbb54444d1859b73fe732c1
Michal Papis <mpapis@gmail.com>
2014-10-06 16:23:20 +0200

    improved tests for hook

--- test/ruby/hooks/hook_test.rb
+++ test/ruby/hooks/hook_test.rb
@@ -19,8 +19,16 @@ describe Ruby::Hooks::Hook do
     Ruby::Hooks::Hook.allocate
   end
 
-  it "extends self" do
+  it "is based on Observable" do
     subject.must_be_kind_of(Observable)
+  end
+
+  it "initializes without params" do
+    subject.send :initialize
+    subject.methods.map(&:to_sym).wont_include :method_2
+  end
+
+  it "extends self" do
     subject.methods.map(&:to_sym).wont_include :method_2
     subject.send :initialize, :extends => HookTestClass::Extra
     subject.methods.map(&:to_sym).must_include :method_2
